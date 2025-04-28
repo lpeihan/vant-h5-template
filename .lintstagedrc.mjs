ba40d7e7e2131fc18fa58b513a1e25313b7de446
Alfred Li <Alfred.Li@ringcentral.com>
2025-04-28 16:07:37 +0800

    add lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,5 +2,5 @@ export default {
   '*.{js,mjs,cjs,ts,vue}': ['npm run eslint', 'npm run prettier', 'git add'],
   '*.{less,css,vue,html}': ['npm run stylelint', 'npm run prettier', 'git add'],
   '*.{js,mjs,cjs,ts,vue,less,css,html}': ['npm run cspell', 'git add'],
-  '*.md, *.json': ['npm run prettier', 'git add'],
+  //   '*.md, *.json': ['npm run prettier', 'git add'],
 };
