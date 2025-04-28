8c6f3d4afca425a70119202d61ad548ed80f6bde
Alfred Li <Alfred.Li@ringcentral.com>
2025-04-28 16:38:14 +0800

    test lint stage

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,4 @@
 export default {
   '*.{js,mjs,cjs,ts,vue}': ['npm run eslint', 'npm run prettier'],
   '*.{less,css,vue,html}': ['npm run stylelint', 'npm run prettier'],
-  '*.md, *.json': ['npm run prettier'],
 };
