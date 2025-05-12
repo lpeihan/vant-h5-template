abbc801be23169bc4f84d988df0cc78f938a67e0
Alfred Li <Alfred.Li@ringcentral.com>
2025-05-12 13:33:57 +0800

    feat: update

--- babel.config.mjs
+++ babel.config.mjs
@@ -3,18 +3,15 @@ export default {
     [
       '@babel/preset-env',
       {
-        useBuiltIns: 'usage', // adds specific imports for polyfills when they are used in each file.
-        modules: 'auto', // preserve ES modules.
-        corejs: { version: 3, proposals: true }, // enable polyfilling of every proposal supported by core-js.
+        useBuiltIns: 'usage',
+        modules: 'auto',
+        corejs: { version: 3, proposals: true },
         targets: {
-          node: 'current', // Ensures compatibility with the current Node.js version used by Jest.
+          node: 'current',
         },
       },
     ],
   ],
-  plugins: [
-    '@babel/plugin-transform-runtime', // enables the re-use of Babel's injected helper code to save on codesize.
-    '@vue/babel-plugin-jsx',
-  ],
+  plugins: ['@babel/plugin-transform-runtime', '@vue/babel-plugin-jsx'],
   exclude: [/core-js/],
 };
